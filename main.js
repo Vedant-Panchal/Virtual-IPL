6860e861ae616204287c78af577ecc393edaf648
Vedant-Panchal <vedkp2421@gmail.com>
2023-07-26 15:44:33 +0530

    All rounder added in html

--- main.js
+++ main.js
@@ -177,6 +177,49 @@ document.addEventListener('DOMContentLoaded', () => {
                               `).join('')}
                             </ul>
                         </div>
+                        <div class="bought-players is-flex is-justify-content-flex-start is-align-items-baseline is-marginless"
+                            style="gap: 10px;margin-bottom: 1em;">
+                            <p class="block">
+                                <strong id="card-body">All rounders</strong>
+                            </p>
+                            <img src="https://cdn-icons-png.flaticon.com/128/1099/1099680.png" id="ball" alt="ball"
+                                    style="width: 0.9em;">
+                            <svg id="bat" viewBox="0 0 460.84737 460.84737" xmlns="http://www.w3.org/2000/svg"
+                                    style="width: 1.5em;margin-left: -5px">
+                                    <path
+                                        d="m460.847656 31.75-25.070312 25.078125-31.761719-31.757813 25.082031-25.070312zm0 0"
+                                        fill="#a85d5d" />
+                                    <path
+                                        d="m378.945312 50.140625 25.070313-25.070313 31.761719 31.757813-25.070313 25.070313zm0 0"
+                                        fill="#7f4545" />
+                                    <path
+                                        d="m353.878906 75.210938 25.066406-25.070313 31.761719 31.757813-25.070312 25.070312zm0 0"
+                                        fill="#a85d5d" />
+                                    <path
+                                        d="m328.808594 100.28125 25.066406-25.070312 31.761719 31.757812-25.070313 25.070312zm0 0"
+                                        fill="#7f4545" />
+                                    <path
+                                        d="m360.566406 132.039062-25.078125 25.070313-31.75-31.75 25.070313-25.078125zm0 0"
+                                        fill="#a85d5d" />
+                                    <path
+                                        d="m352.425781 190.320312-260.136719 260.140626c-13.847656 13.847656-36.296874 13.847656-50.140624 0l-31.761719-31.761719c-13.847657-13.84375-13.847657-36.296875 0-50.140625l260.140625-260.136719 25.070312 25.066406-.21875.222657-76.050781 107.808593 107.808594-76.050781.21875-.21875zm0 0"
+                                        fill="#ffd2a6" />
+                                    <path
+                                        d="m327.355469 165.25-.21875.21875-107.808594 76.050781 76.050781-107.808593.21875-.222657zm0 0"
+                                        fill="#7f4545" />
+                                </svg>
+                        </div>
+                        <hr style="margin-top: -20px;">
+                        <div class="content">
+                            <ul class="is-left has-text-left is-flex-direction-column is-justify-content-center is-align-items-start is-marginless" style="list-style: decimal inside;">
+                            ${team.allRounders.map(player => `
+                            <div class="is-flex is-justify-content-space-between">
+                            <li>${player.playerName}</li>
+                            <div>Price : ₹<span>  ${parseInt(parsePrice(player.playerPrice)).toLocaleString("en-IN")}</span></div>
+                            </div>
+                              `).join('')}
+                            </ul>
+                        </div>
                     </div>
                 `
           teamGroupElement.appendChild(teamContainer);
